5005fff555bdaf760918b73068642441b201ad11
fj0rdingz <haruchin.mi@gmail.com>
2020-09-19 20:39:03 +0900

    hai

---
+++ abc178/c.cpp
@@ -0,0 +1,122 @@
+// lcmとか__builtin_popcountとかはg++ -std=c++17 default.cppみたいなかんじで
+// g++ hoge.cpp -std=c++17 -I . でコンパイルできる
+// -fsanitize=undefinedでオーバーフロー検出
+#include <bits/stdc++.h>
+//#include <atcoder/all>
+#define mod 1000000007
+#define INF LLONG_MAX
+#define ll long long
+#define ln cout<<endl
+#define Yes cout<<"Yes"<<endl
+#define NO cout<<"NO"<<endl
+#define YES cout<<"YES"<<endl
+#define No cout<<"No"<<endl
+#define REP(i,m,n) for(ll i=(ll)(m);i<(ll)(n);i++)
+#define rep(i,n) REP(i,0,n)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+using namespace std;
+//using namespace atcoder;
+typedef pair<ll,ll> P;
+ll dx[4]={1,0,-1,0};
+ll dy[4]={0,1,0,-1};
+class mint {
+    long long x;
+public:
+    mint(long long x=0) : x((x%mod+mod)%mod) {}
+    mint operator-() const { 
+      return mint(-x);
+    }
+    mint& operator+=(const mint& a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint& a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const  mint& a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+    mint operator+(const mint& a) const {
+        mint res(*this);
+        return res+=a;
+    }
+    mint operator-(const mint& a) const {
+        mint res(*this);
+        return res-=a;
+    }
+    mint operator*(const mint& a) const {
+        mint res(*this);
+        return res*=a;
+    }
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+    // for prime mod
+    mint inv() const {
+        return pow(mod-2);
+    }
+    mint& operator/=(const mint& a) {
+        return (*this) *= a.inv();
+    }
+    mint operator/(const mint& a) const {
+        mint res(*this);
+        return res/=a;
+    }
+
+    friend ostream& operator<<(ostream& os, const mint& m){
+        os << m.x;
+        return os;
+    }
+};
+
+
+const int MAX = 510000;
+const int MOD = 1000000007;
+
+long long fac[MAX], finv[MAX], inv[MAX];
+
+// テーブルを作る前処理
+void cominit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++){
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+// 二項係数計算
+long long com(int n, int k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+int main() {
+    cin.tie(0);
+   	ios::sync_with_stdio(false);
+
+    ll a,b,c,d,m,n,k,x,y,maxi=0,f=0,mini=INF;
+    ll sum=0;
+    string str;
+    cin>>n;
+    cominit();
+    sum=com(n,2)%mod;
+    sum*=2;
+    sum%=mod;
+    rep(i,n-2) {
+        sum*=10;
+        sum=sum%mod;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
---
+++ abc178/d.cpp
@@ -0,0 +1,121 @@
+// lcmとか__builtin_popcountとかはg++ -std=c++17 default.cppみたいなかんじで
+// g++ hoge.cpp -std=c++17 -I . でコンパイルできる
+// -fsanitize=undefinedでオーバーフロー検出
+#include <bits/stdc++.h>
+//#include <atcoder/all>
+#define mod 1000000007
+#define INF LLONG_MAX
+#define ll long long
+#define ln cout<<endl
+#define Yes cout<<"Yes"<<endl
+#define NO cout<<"NO"<<endl
+#define YES cout<<"YES"<<endl
+#define No cout<<"No"<<endl
+#define REP(i,m,n) for(ll i=(ll)(m);i<(ll)(n);i++)
+#define rep(i,n) REP(i,0,n)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+using namespace std;
+//using namespace atcoder;
+typedef pair<ll,ll> P;
+ll dx[4]={1,0,-1,0};
+ll dy[4]={0,1,0,-1};
+
+class mint {
+    long long x;
+public:
+    mint(long long x=0) : x((x%mod+mod)%mod) {}
+    mint operator-() const { 
+      return mint(-x);
+    }
+    mint& operator+=(const mint& a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint& a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const  mint& a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+    mint operator+(const mint& a) const {
+        mint res(*this);
+        return res+=a;
+    }
+    mint operator-(const mint& a) const {
+        mint res(*this);
+        return res-=a;
+    }
+    mint operator*(const mint& a) const {
+        mint res(*this);
+        return res*=a;
+    }
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+    // for prime mod
+    mint inv() const {
+        return pow(mod-2);
+    }
+    mint& operator/=(const mint& a) {
+        return (*this) *= a.inv();
+    }
+    mint operator/(const mint& a) const {
+        mint res(*this);
+        return res/=a;
+    }
+
+    friend ostream& operator<<(ostream& os, const mint& m){
+        os << m.x;
+        return os;
+    }
+};
+
+int main() {
+    cin.tie(0);
+   	ios::sync_with_stdio(false);
+
+    ll a,b,c,d,m,n,k,x,y,maxi=0,f=0,mini=INF;
+    mint sum=0;
+    string str;
+    cin>>n;
+    mint dp[n+5][n+5];
+    rep(i,n){
+        dp[0][i]=0;
+    }
+    rep(i,n){
+        dp[1][i]=1;
+    }
+    for(ll i=2;i<n;i++){
+        rep(j,n) dp[i][j]=0;
+    }
+    if(n<=2){
+        cout<<0<<endl;
+        return 0;
+    }
+    ll start=2;
+    for(ll i=2;i<n;i++){
+        for(ll j=start;j<n;j++){
+            dp[i][j]=dp[i-1][j-3]+dp[i][j-1];
+        }
+        start+=3;
+    }
+    rep(i,n){
+        rep(j,n){
+           // cout<<dp[i][j]<<" ";
+        }
+       // ln;
+    }
+    rep(i,n){
+        sum+=dp[i][n-3];
+    }
+    cout<<sum<<endl;
+
+    return 0;
+}
